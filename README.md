9fe9b53a53e6a82be478a676ea8a4d771685fe5c
theeluwin <theeluwin@gmail.com>
2016-05-03 17:01:15 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# textrankr
-TextRank for Korean.
+TextRank for Korean
+==========
+
+Reorder sentences using [TextRank][1] algorithm.
+
+
+Usage
+-----
+
+```python
+from textrankr import TextRank
+
+textrank = TextRank(your_text_here)
+print textrank.reordered[0].text
+```
+
+[1]: http://digital.library.unt.edu/ark:/67531/metadc30962/
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='textrankr',
+    packages=['textrankr'],
+    version='0.1',
+    description='TextRank for Korean',
+    author='theeluwin',
+    author_email='theeluwin@gmail.com',
+    url='https://github.com/theeluwin/textrankr',
+    download_url='https://github.com/theeluwin/textrankr/tarball/0.1',
+    keywords=['textrank', 'korean', 'networkx', 'konlpy'],
+    classifiers=[],
+)
