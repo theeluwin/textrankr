6145a5725fcee7bc5213bcbe885c73b190e9f4cb
jaeyoung.choi <lactrious@gmail.com>
2020-08-28 06:18:48 +0900

    Inject phrases function from outside

--- textrankr/sentence.py
+++ textrankr/sentence.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from collections import Counter
-from konlpy.tag import Okt
 
 
 class Sentence(object):
 
-    okt = Okt()
-
-    def __init__(self, text, index=0):
+    def __init__(self, phraser, text, index=0):
         self.index = index
         self.text = text.strip()
-        self.tokens = self.okt.phrases(self.text)
+        self.tokens = phraser(self.text)
         self.bow = Counter(self.tokens)
 
     def __str__(self):
